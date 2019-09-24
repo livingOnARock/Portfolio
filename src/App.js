45dd98c5eca4dc0165ae99643d214e5299fd44cb
livingOnARock <41513380+livingOnARock@users.noreply.github.com>
2019-09-23 23:33:50 -0500

    changed design of app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import React, { Fragment } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import RecentWork from "./components/RecentWork";
-import Contact from "./components/Contact";
-import Navbar from "./components/AppNavbar";
+import React from "react";
+import "./App.scss";
 
-import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
 
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Fragment>
-        <Navbar style={{ position: "fixed" }} />
-        <div className="App">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/recentWork" component={RecentWork} />
-            <Route exact path="/contact" component={Contact} />
-          </Switch>
-        </div>
-      </Fragment>
-    </BrowserRouter>
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
   );
-};
+}
 
 export default App;
